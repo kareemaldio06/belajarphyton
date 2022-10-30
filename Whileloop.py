6265fee91dbb61deb24dbd8a241a01d323ebfb64
kareemaldio06 <aldiokareem@gmail.com>
2022-10-30 15:01:30 +0700

    30.10.22
    Pelajaran LOOP

---
+++ Whileloop.py
@@ -0,0 +1,6 @@
+Num = int(input('insert number'))
+Numstart = 1
+
+while Numstart <= Num:
+    print(f'nilai {Numstart}')
+    Numstart = Numstart + 1
---
+++ forloop.py
@@ -0,0 +1,4 @@
+Num = int(input('insert number'))
+
+for i in range(Num):
+    print(f'Nilai ke {i}')
